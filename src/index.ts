3d144c81508974360b016ba1ea2b7b83953ae992
Akash NP <akashnp1998@gmail.com>
2023-11-02 12:34:51 +0400

    feat: setup standardized response funcs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World")
+import type { Response } from "express";
+
+export function createSuccessResponse(
+  res: Response,
+  code: number = 200,
+  data: any = {}
+) {
+  res.status(code).json({status: 'success', data})
+}
+
+export function createErrorResponse(
+  res: Response,
+  code: number = 400,
+  message: string = "",
+  error: any = {}
+) {
+  res.status(code).json({status: 'fail', message, error})
+}
+
+export function createFailureResponse(
+  res: Response,
+  code: number = 500,
+  message: string = "",
+  error: any = {}
+) {
+  res.status(code).json({status: 'fail', message, error})
+}
